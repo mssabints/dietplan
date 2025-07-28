dc2085f9c509ff7b04eb9c031b4773ce2a17c753
sabin sunny <sabin.jb.24@gmail.com>
2025-07-28 23:23:03 +0100

    update ui and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,35 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+      colors: {
+        orange: {
+          200: '#fed7aa',
+          500: '#f97316',
+          600: '#ea580c',
+          800: '#c2410c',
+          900: '#9a3412',
+        },
+        gray: {
+          200: '#e5e7eb',
+          600: '#4b5563',
+          800: '#1f2937',
+        },
+        green: {
+          600: '#16a34a',
+        },
+        yellow: {
+          600: '#ca8a04',
+        },
+        red: {
+          600: '#dc2626',
+        },
+        blue: {
+          600: '#2563eb',
+        },
+        brown: {
+          600: '#8b4513',
+        },
+      },
     },
   },
   plugins: [],
